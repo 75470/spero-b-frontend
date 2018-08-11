4bcd35d6dca9a3564c4c3341f98fd47e843c7eb4
Preeti bhonsle <pre@deakin.edu.au>
2018-08-11 17:28:21 +1000

    initial commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+(function(){
+    var settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "http://localhost:3000/getCommits?repoName=Spero-b&autherName=T37A",
+        "method": "GET"
+      }
+      $.ajax(settings).done(function (response) {
+        let i = 1;
+        response.forEach(element => {
+            $("#git-info").append(`<tr>
+                <td>${i++}</td>
+                <td>${element.commit.author.name}</td>
+                <td>${element.commit.author.email}</td>
+                <td>${element.commit.author.date}</td>
+                <td>${element.commit.message}</td>
+            </tr>`);
+        });
+    });
+})();
